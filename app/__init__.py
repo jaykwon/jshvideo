15e8b8c9b83fe50e34ad791b79126a4af2cc7d85
Justin Shelton <justin@thehumangeo.com>
2014-08-18 20:17:37 -0400

    First take with Disqus

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.httpauth import HTTPDigestAuth
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
 db = SQLAlchemy(app)
+auth = HTTPDigestAuth()
 
 
 from app.routes import index
-
--- config.py
+++ config.py
@@ -6,3 +6,5 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 VIDEOS_DIRECTORY = 'app/static/media/videos'
 FRAMES_DIRECTORY = 'app/static/media/frames'
+
+SECRET_KEY = ')\xf5\x96\xc7\x8e\xc0|Mt\xca@\xf0s\xfd\xce\xc8\xe1\xf5\xa8V\x973\xcd2'
