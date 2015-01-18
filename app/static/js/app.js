2f4bde301e98d5069aabd605ab139eb131dbe3ec
Justin Shelton <justin@thehumangeo.com>
2015-01-18 01:16:50 -0500

    A new start on develop

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,6 @@
 // Declare app level module which depends on filters, and services
-var app = angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'underscore', "mediaPlayer"])
-  .config(['$routeProvider', function ($routeProvider) {
+var app = angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.date', 'underscore', "mediaPlayer", 'ngDisqus', 'satellizer'])
+  .config(['$routeProvider', '$locationProvider', '$disqusProvider', '$authProvider', function ($routeProvider, $locationProvider, $disqusProvider, $authProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/home/home.html',
@@ -9,4 +9,12 @@ var app = angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ui.da
         templateUrl: 'views/home/detail.html',
         controller: 'DetailController'})
       .otherwise({redirectTo: '/'});
+    $locationProvider
+      .html5Mode(false)
+      .hashPrefix('!');
+    $disqusProvider
+      .setShortname('mightynorth');
+    $authProvider.twitter({
+        url: '/auth/twitter'
+    });
   }]);
--- app/static/js/home/home-controller.js
+++ app/static/js/home/home-controller.js
@@ -51,3 +51,9 @@ app.controller('DetailController', ['$scope', '$routeParams', 'videoFactory', fu
         ]
     });
 }]);
+
+app.controller('LoginController', function($scope, $auth) {
+    $scope.authenticate = function(provider) {
+        $auth.authenticate(provider);
+    };
+});
